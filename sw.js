8003b8bfd7c68d12cd2e33a8cb7045ad9c0bd33d
Brothersadega <61800320+Thhirodrigues@users.noreply.github.com>
2026-05-26 20:22:12 -0300

    Enhance service worker notification handling

--- sw.js
+++ sw.js
@@ -1,5 +1,3 @@
-self.addEventListener('notificationclick', e => { e.notification.close(); });
-self.addEventListener('push', e => {});
 self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => {});
-self.show = (t,b) => self.registration.showNotification(t,{body:b,icon:'icon-192.png',vibrate:[1000,500,1000,500,1000],requireInteraction:true});
+self.addEventListener('activate', e => self.clients.claim());
+self.addEventListener('notificationclick', e => { e.notification.close(); e.waitUntil(clients.openWindow('/forex-assist/')); });

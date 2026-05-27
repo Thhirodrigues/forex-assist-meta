f1566277226cf7ed81fb01fb681c02bd82093c4f
Brothersadega <61800320+Thhirodrigues@users.noreply.github.com>
2026-05-26 21:10:01 -0300

    Add service worker event listeners for install and activate

---
+++ sw.js
@@ -0,0 +1,3 @@
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => self.clients.claim());
+self.addEventListener('notificationclick', e => { e.notification.close(); e.waitUntil(clients.openWindow('/forex-assist/')); });

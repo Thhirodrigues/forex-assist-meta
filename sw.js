7c4a350fcbf37eb351cb51fb68fcaac1e2c71e00
Brothersadega <61800320+Thhirodrigues@users.noreply.github.com>
2026-05-28 14:38:27 -0300

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('notificationclick', e => {
-  e.notification.close();
-  e.waitUntil(clients.openWindow('intent://#Intent;package=com.xm.webapp;scheme=xm;end'));
-});
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
